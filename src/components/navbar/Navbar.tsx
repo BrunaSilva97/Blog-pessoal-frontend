786697142c13480453e29dfb0113ce940a01c070
Bruna Roberta <brunarob97@gmail.com>
2025-01-20 15:37:37 -0300

    React Router Dom, cadastro e login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@phosphor-icons/react"
+
 function Navbar() {
     return (
         <>
@@ -5,7 +7,7 @@ function Navbar() {
             			   bg-purple-800 text-white'>
             
                 <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
                         Postagens |
